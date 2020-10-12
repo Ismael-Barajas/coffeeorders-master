f03b21a35499b9c8e622a1464f1018f2386c0065
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2020-10-11 17:42:55 -0700

    added eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/* jshint esversion: 6 */
+export default {
+    "env": {
+      "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+      "indent": [
+        "error",
+        2
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "double"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ]
+    }
+  };
+  
